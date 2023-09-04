2a3b415c39f4b98fc376f2ec932b3bbe2c8f8583
Aahil Ali <aahil0218@gmail.com>
2023-09-04 02:11:03 -0500

    Update UserDatabase.h

--- UserDatabase.h
+++ UserDatabase.h
@@ -2,6 +2,8 @@
 #define USERDATABASE_INCLUDED
 
 #include <string>
+#include <vector>
+#include "treemm.h"
 
 class User;
 
@@ -9,10 +11,13 @@ class UserDatabase
 {
   public:
     UserDatabase();
+    ~UserDatabase();
     bool load(const std::string& filename);
     User* get_user_from_email(const std::string& email) const;
 
   private:
+    std::vector<User*> m_users;
+    TreeMultimap<std::string, User*> m_useremail_tree;
 };
 
 #endif // USERDATABASE_INCLUDED
